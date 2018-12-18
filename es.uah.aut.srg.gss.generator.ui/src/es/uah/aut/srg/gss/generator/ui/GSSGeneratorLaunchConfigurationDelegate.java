d177d848eb7ffec5c7912391c736123cbf667c68
Pablo Parra <parraman@gmail.com>
2018-12-18 15:58:27 +0100

    Minor fix
    Now the project folder is refreshed every time a new resource is
    generated.

--- es.uah.aut.srg.gss.generator.ui/src/es/uah/aut/srg/gss/generator/ui/GSSGeneratorLaunchConfigurationDelegate.java
+++ es.uah.aut.srg.gss.generator.ui/src/es/uah/aut/srg/gss/generator/ui/GSSGeneratorLaunchConfigurationDelegate.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.util.Collection;
 
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -59,20 +61,23 @@ public class GSSGeneratorLaunchConfigurationDelegate implements ILaunchConfigura
 			throw new CoreException(new Status(
 				IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e));
 		}
+
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource folder = root.findMember(output);
+		
+		if (folder == null || (folder instanceof IFolder) == false) {
+			throw new CoreException(new Status(
+					IStatus.ERROR, Activator.PLUGIN_ID, "Resource '" + output + "' not found!"));
+		}
+
 		
 		for (GSSTmTcFormatTmTcFormat gssTmTcFormatTmTcFormat : formats) {
-		
-			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-			IResource folder = root.findMember(output);
-			
-			if (folder == null || (folder instanceof IFolder) == false) {
-				throw new CoreException(new Status(
-						IStatus.ERROR, Activator.PLUGIN_ID, "Resource '" + output + "' not found!"));
-			}
 		
 			XpandGeneratorUtil.generate(folder.getLocation().toPortableString(), gssTmTcFormatTmTcFormat,
 					"es::uah::aut::srg::gss::generator::templates::tm_tc_formatSerializer::Serializer", 
 					false, gssTmTcFormatTmTcFormat.getName() + ".gss_tm_tc_format");
+			
+			folder.getProject().refreshLocal(IProject.DEPTH_INFINITE, new NullProgressMonitor());
 			
 			// If everything went fine, we should be able to open the new file
 			
