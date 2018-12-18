97fd6f8946604c64ada9d0d4598fb145a827041c
Pablo Parra <parraman@gmail.com>
2018-12-18 12:51:10 +0100

    Added code to generate the XMI files
    Now the process of extracting the tm_tc_format files from the database
    also generates the corresponding XMI files. The helper function
    convertReferences() has been extracted from the original GenerateXML
    class to a new location so that it can be also used in the new generator
    process.

--- es.uah.aut.srg.gss.generator.ui/src/es/uah/aut/srg/gss/generator/ui/GSSGeneratorLaunchConfigurationDelegate.java
+++ es.uah.aut.srg.gss.generator.ui/src/es/uah/aut/srg/gss/generator/ui/GSSGeneratorLaunchConfigurationDelegate.java
@@ -3,6 +3,7 @@ package es.uah.aut.srg.gss.generator.ui;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -13,15 +14,23 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtend.typesystem.emf.EmfRegistryMetaModel;
 
+import es.uah.aut.srg.gss.common.GSSModelFile;
 import es.uah.aut.srg.gss.common.commonPackage;
 import es.uah.aut.srg.gss.generator.GSSGenerator;
 import es.uah.aut.srg.gss.generator.util.XpandGeneratorUtil;
 import es.uah.aut.srg.gss.tm_tc_format.GSSTmTcFormatTmTcFormat;
 import es.uah.aut.srg.gss.tm_tc_format.tm_tc_formatPackage;
+import es.uah.aut.srg.gss.xtext.xml.XMLGeneratorUtil;
 
 
 public class GSSGeneratorLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
@@ -56,7 +65,7 @@ public class GSSGeneratorLaunchConfigurationDelegate implements ILaunchConfigura
 			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 			IResource folder = root.findMember(output);
 			
-			if (folder == null) {
+			if (folder == null || (folder instanceof IFolder) == false) {
 				throw new CoreException(new Status(
 						IStatus.ERROR, Activator.PLUGIN_ID, "Resource '" + output + "' not found!"));
 			}
@@ -64,6 +73,39 @@ public class GSSGeneratorLaunchConfigurationDelegate implements ILaunchConfigura
 			XpandGeneratorUtil.generate(folder.getLocation().toPortableString(), gssTmTcFormatTmTcFormat,
 					"es::uah::aut::srg::gss::generator::templates::tm_tc_formatSerializer::Serializer", 
 					false, gssTmTcFormatTmTcFormat.getName() + ".gss_tm_tc_format");
+			
+			// If everything went fine, we should be able to open the new file
+			
+			ResourceSet resourceSet = new ResourceSetImpl();
+			
+			IResource newFile = ((IFolder)folder).findMember(gssTmTcFormatTmTcFormat.getName() + ".gss_tm_tc_format");
+			
+			if (newFile == null) {
+				throw new CoreException(new Status(
+						IStatus.ERROR, Activator.PLUGIN_ID, "Resource '" + newFile + "' not found!"));
+			}
+			
+			Resource xtextResource = 
+					resourceSet.getResource(URI.createPlatformResourceURI(newFile.getFullPath().toString(), true), true);
+				
+			EObject model = xtextResource.getContents().get(0);
+				
+			XMLGeneratorUtil.convertReferences(model);
+
+			EObject outputModel = EcoreUtil.copy(((GSSModelFile)model).getElement());
+			String pathName = 
+					newFile.getFullPath().removeFileExtension().addFileExtension("xmi").toString();
+				
+			final Resource xmiResource = resourceSet.createResource(URI.createPlatformResourceURI(pathName, true));
+												
+			xmiResource.getContents().add(outputModel);
+			
+			try {
+				xmiResource.save(null);
+			} catch (IOException e) {
+				throw new CoreException(new Status(
+						IStatus.ERROR, Activator.PLUGIN_ID, "Error when generating '" + newFile));
+			}
 			
 		}
 		
--- es.uah.aut.srg.gss.xtext.ui/src/es/uah/aut/srg/gss/xtext/ui/handlers/GenerateXML.java
+++ es.uah.aut.srg.gss.xtext.ui/src/es/uah/aut/srg/gss/xtext/ui/handlers/GenerateXML.java
@@ -12,9 +12,6 @@ package es.uah.aut.srg.gss.xtext.ui.handlers;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -24,7 +21,6 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -40,6 +36,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import es.uah.aut.srg.gss.common.GSSModelFile;
+import es.uah.aut.srg.gss.xtext.xml.XMLGeneratorUtil;
 
 public class GenerateXML extends AbstractHandler {
 	
@@ -47,57 +44,6 @@ public class GenerateXML extends AbstractHandler {
 	
 	protected ResourceSet resourceSet = new ResourceSetImpl();
 	
-	protected void convertReferences(EObject model) {
-		
-		List<Resource> doneResources = new ArrayList<Resource>();
-		
-		for (TreeIterator<EObject> it = model.eAllContents(); it.hasNext(); ) {
-			EObject eObject = it.next();
-			
-			for (Iterator<EObject> featureIterator =  eObject.eCrossReferences().iterator(); featureIterator.hasNext(); )
-			{
-			    EObject referredObject = featureIterator.next();
-			    Resource resource = referredObject.eResource();
-
-			    // If the object is in the same resource, continue to the next one
-			    // NOTE: This is not supposed to happen, since you should not be able to define
-			    // more than one object on the same file
-			    
-			    if (resource == model.eResource()) {
-			    	continue;
-			    }
-			    
-			    // Check if the fragment of the container resource is of type platform
-			    // If is not, then we do not know how to proceed => continue
-			    if (resource.getURI().isPlatformResource() == false) {
-			    	continue;
-			    }
-			    
-			    if (doneResources.contains(resource)) {
-			    	continue;
-			    }
-			    
-			    doneResources.add(resource);
-			    
-			    EObject root = EcoreUtil.getRootContainer(referredObject);
-			    
-			    if ((root instanceof GSSModelFile) == false) {
-			    	continue;
-			    }
-			    
-			    resource.getContents().add(((GSSModelFile)root).getElement());
-   
-			    EcoreUtil.delete(root);
-			    
-			    URI newURI = referredObject.eResource().getURI().trimFileExtension().appendFileExtension("xmi");
-			    referredObject.eResource().setURI(newURI);
-			    
-			}
-
-		}
-		
-	}
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
@@ -117,7 +63,7 @@ public class GenerateXML extends AbstractHandler {
 				
 				EObject model = xtextResource.getContents().get(0);
 				
-				this.convertReferences(model);
+				XMLGeneratorUtil.convertReferences(model);
 
 				EObject outputModel = EcoreUtil.copy(((GSSModelFile)model).getElement());
 				String pathName = 
